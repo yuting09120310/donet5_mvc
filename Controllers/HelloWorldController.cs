ceb2a0ad3cb2d5499792f0aa463ad44344a0b3e0
alex <yuting09120310@hotmail.com>
2021-11-17 22:45:34 +0800

    加入專案檔案。

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using my_first_mvc.Models;
+using static my_first_mvc.Models.Dbmanager;
+
+namespace my_first_mvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorldController
+
+        //進到HelloWorld/index的事件
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Dbmanager dbmanager = new Dbmanager();
+            List<Dbmanager.Account> accounts = dbmanager.GetAccounts("物料");
+            ViewBag.accounts = accounts;
+            return View();
+        }
+
+
+        //進到CreateAccount的事件
+        [HttpGet]
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        //CreateAccount按下新增後的事件
+        [HttpPost]
+        public ActionResult CreateAccount(Dbmanager.Account account)
+        {
+            Dbmanager dbmanager = new Dbmanager();
+            try
+            {
+                dbmanager.newAccount(account);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return RedirectToAction("index");
+        }
+
+        //抓取edit的id內的修改資料 丟到以下的post事件
+        [HttpGet]
+        public ActionResult EditAccount(string ID)
+        {
+            Dbmanager dbmanager = new Dbmanager();
+            Account account = dbmanager.getAccount(ID);
+            return View(account);
+        }
+
+        //到資料庫修改數據
+        [HttpPost]
+        public ActionResult EditAccount(Dbmanager.Account account)
+        {
+            Dbmanager dbmanager = new Dbmanager();
+            dbmanager.EditAccount(account);
+            return RedirectToAction("index");
+        }
+
+        //刪除資料
+        public ActionResult DelAccount(string ID)
+        {
+            Dbmanager dbmanager = new Dbmanager();
+            dbmanager.DelAccount(ID);
+            return RedirectToAction("index");
+        }
+
+        // GET: HelloWorldController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HelloWorldController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HelloWorldController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HelloWorldController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Dbmanager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_first_mvc.Models
+{
+    public class Dbmanager
+    {
+        private readonly string dbconn = @"Data Source=DESKTOP-OCAV8JE\SQLEXPRESS;Initial Catalog=Production;User ID=sa;Password=chi";
+
+        public class Account
+        {
+            public string ID { get; set; }
+            public string password { get; set; }
+            public string name { get; set; }
+            public string deptname { get; set; }
+        }
+
+        public List<Account> GetAccounts(string dept)
+        {
+            List<Account> accounts = new List<Account>();
+            SqlConnection sqlConn = new SqlConnection(dbconn);
+            SqlCommand sqlComm = new SqlCommand($"SELECT * FROM Account where deptname = '{dept}'");
+            sqlComm.Connection = sqlConn;
+            sqlConn.Open();
+
+            SqlDataReader reader = sqlComm.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Account account = new Account
+                    {
+                        ID = reader.GetString(reader.GetOrdinal("ID")),
+                        password = reader.GetString(reader.GetOrdinal("password")),
+                        name = reader.GetString(reader.GetOrdinal("name")),
+                        deptname = reader.GetString(reader.GetOrdinal("deptname")),
+                    };
+                    accounts.Add(account);
+                }
+            }
+            else
+            {
+                Console.WriteLine("資料庫為空！");
+            }
+            sqlConn.Close();
+            return accounts;
+        }
+
+        public void newAccount(Dbmanager.Account account)
+        {
+            SqlConnection sqlConn = new SqlConnection(dbconn);
+            SqlCommand sqlComm = new SqlCommand($"INSERT INTO account values ('{account.ID}','{account.password}','{account.name}','{account.deptname}')");
+            sqlComm.Connection = sqlConn;
+
+            //sqlcomm.Parameters.Add(new SqlParameter("@ID", account.ID));
+            //sqlcomm.Parameters.Add(new SqlParameter("@password", account.password));
+            //sqlcomm.Parameters.Add(new SqlParameter("@name", account.name));
+            //sqlcomm.Parameters.Add(new SqlParameter("@deptname", account.deptname));
+            sqlConn.Open();
+            sqlComm.ExecuteNonQuery();
+            sqlConn.Close();
+        }
+
+        public Account getAccount(string id)
+        {
+            Account account = new Account();
+            SqlConnection sqlConn = new SqlConnection(dbconn);
+            SqlCommand sqlComm = new SqlCommand($"SELECT * FROM Account where id = '{id}'");
+            sqlComm.Connection = sqlConn;
+            sqlConn.Open();
+
+            SqlDataReader reader = sqlComm.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    account = new Account
+                    {
+                        ID = reader.GetString(reader.GetOrdinal("ID")),
+                        password = reader.GetString(reader.GetOrdinal("password")),
+                        name = reader.GetString(reader.GetOrdinal("name")),
+                        deptname = reader.GetString(reader.GetOrdinal("deptname")),
+                    };
+                }
+            }
+            sqlConn.Close();
+            return account; 
+        }
+
+        public Account EditAccount(Dbmanager.Account account)
+        {
+            SqlConnection sqlConn = new SqlConnection(dbconn);
+            SqlCommand sqlComm = new SqlCommand($"Update account set password='{account.password}', name='{account.name}', deptname='{account.deptname}' where ID = '{account.ID}'");
+            sqlComm.Connection = sqlConn;
+            sqlConn.Open();
+            sqlComm.ExecuteNonQuery();
+            sqlConn.Close();
+            return account;
+        }
+
+        public void DelAccount(string ID)
+        {
+            SqlConnection sqlConn = new SqlConnection(dbconn);
+            SqlCommand sqlComm = new SqlCommand($"Delete account where ID = '{ID}'");
+            sqlComm.Connection = sqlConn;
+            sqlConn.Open();
+            sqlComm.ExecuteNonQuery();
+            sqlConn.Close();
+        }
+    }
+}
